f979d847075b255197ba0fb5e7d9784f7d24f437
Scott Sloan <scott.sloan@agropur.com>
2015-10-07 14:15:06 -0500

    Adding http Client package

--- ExampleAurelia/config.js
+++ ExampleAurelia/config.js
@@ -25,6 +25,7 @@ System.config({
     "aurelia-framework": "github:aurelia/framework@0.16.0",
     "aurelia-history": "github:aurelia/history@0.7.0",
     "aurelia-history-browser": "github:aurelia/history-browser@0.8.0",
+    "aurelia-http-client": "github:aurelia/http-client@0.11.0",
     "aurelia-loader": "github:aurelia/loader@0.9.0",
     "aurelia-loader-default": "github:aurelia/loader-default@0.10.0",
     "aurelia-logging": "github:aurelia/logging@0.7.0",
@@ -160,8 +161,19 @@ System.config({
       "aurelia-task-queue": "github:aurelia/task-queue@0.7.0",
       "core-js": "npm:core-js@0.9.18"
     },
-    "github:jspm/nodelibs-process@0.1.1": {
-      "process": "npm:process@0.10.1"
+    "github:jdanyow/aurelia-computed@0.6.0": {
+      "aurelia-binding": "github:aurelia/binding@0.9.1",
+      "aurelia-logging": "github:aurelia/logging@0.7.0",
+      "core-js": "npm:core-js@0.9.18"
+    },
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.2"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
     },
     "github:paulvanbladel/aurelia-auth@0.9.17": {
       "aurelia-framework": "github:aurelia/framework@0.16.0",
@@ -171,16 +183,29 @@ System.config({
     "github:twbs/bootstrap@3.3.5": {
       "jquery": "github:components/jquery@2.1.4"
     },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
     "npm:babel-runtime@5.8.20": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:core-js@0.9.18": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.1",
+      "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:font-awesome@4.4.0": {
       "css": "github:systemjs/plugin-css@0.1.16"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:process@0.11.2": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
     }
   }
 });
