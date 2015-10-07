981eeb2cbb5bbcbeb50eace5509fb1250d52c51d
Scott Sloan <scott.sloan@agropur.com>
2015-10-07 14:13:01 -0500

    Added profile information to navbar

--- ExampleAurelia/src/nav-bar.js
+++ ExampleAurelia/src/nav-bar.js
@@ -1,21 +1,34 @@
 ﻿import {bindable} from 'aurelia-framework';  
 import {inject} from 'aurelia-framework';  
+import {computedFrom} from 'aurelia-framework';  
 import {AuthService} from 'aurelia-auth';
 
 @inject(AuthService)
-
 export class NavBar {  
     // User isn't authenticated by default
     _isAuthenticated = false;
+    profile = {}; 
     @bindable router = null;
-
-    constructor(auth) {
-        this.auth = auth;
-    };
 
     // We can check if the user is authenticated
     // to conditionally hide or show nav bar items
     get isAuthenticated() {
-        return this.auth.isAuthenticated();
+        this._isAuthenticated = this.auth.isAuthenticated();
+        return this._isAuthenticated;
+    };
+
+    @computedFrom('profile')
+    get displayName(){
+        return `${this.profile.given_name} ${this.profile.family_name}`;
+    }
+
+    constructor(auth) {
+        this.auth = auth;      
+        this._isAuthenticated = this.auth.isAuthenticated();
+
+        if(this._isAuthenticated)
+        {
+            this.auth.getMe().then(data => { this.profile = data});
+        }
     };
 }
